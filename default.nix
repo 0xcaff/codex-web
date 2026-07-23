78ba0ac0e4e597ebedb53d2c2eb710c9696d8e65
martin <martincharles07@gmail.com>
2026-07-23 18:34:25 -0500

    Revert "remove codex cli override"
    This reverts commit 8bbeaf9efb3769865aef442420f7e68c99567037.

--- default.nix
+++ default.nix
@@ -138,6 +138,11 @@ flake-utils.lib.eachSystem systems (
             rm -rf "$addon/build"
             ln -s ${betterSqlite3Native}/build "$addon/build"
           '';
+
+          postFixup = ''
+            wrapProgram "$out/bin/codex-web" \
+              --set CODEX_CLI_PATH ${pkgs.lib.getExe' codex "codex"}
+          '';
         };
 
         codex_remote_proxy = pkgs.writeShellApplication {
